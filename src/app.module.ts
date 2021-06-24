09075f6204d2a3a1b4f56136e61553aae9a92112
trepichio <trepichio@gmail.com>
2021-06-24 04:33:47 -0300

    connect to mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import config from './config/database';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ expandVariables: true }),
+    MongooseModule.forRoot(process.env.DATABASE_URL, config),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
