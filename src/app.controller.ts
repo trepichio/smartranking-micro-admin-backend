769b1bb71a81066e6c6bedcd2b8076841e0b3ed0
João Trepichio <trepichio@gmail.com>
2021-07-05 12:14:00 -0300

    consumes createCategory as Event Pattern (async) add logger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
+import { CategoryInterface } from './interfaces/categories/category.interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  private readonly logger = new Logger(AppController.name);
+
+  @EventPattern('create-category')
+  async createCategory(@Payload() category: CategoryInterface) {
+    this.logger.log(`Category: ${JSON.stringify(category)}`);
+
+    await this.appService.createCategory(category);
+  }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CategoryInterface } from './interfaces/categories/category.interface';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectModel('Category')
+    private readonly categoryModel: Model<CategoryInterface>,
+  ) {}
+
+  private readonly logger = new Logger(AppService.name);
+
+  async createCategory(dto: CategoryInterface): Promise<CategoryInterface> {
+    try {
+      const categoryCreated = new this.categoryModel(dto);
+      return await categoryCreated.save();
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+      throw new RpcException(error.message);
+    }
   }
 }
